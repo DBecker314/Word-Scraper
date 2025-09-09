f254816dd8b3fb824d5cb5085a558e6a3129ac19
Dbecker314 <beckerdominicr@gmail.com>
2025-09-09 13:48:50 -0400

    Test Word Parse

--- Word-Scraper.py
+++ Word-Scraper.py
@@ -1,1 +1,5 @@
-from docx import Document
+from docx.api import Document
+document = Document("Proxy Review Sheet.docx")
+for p in document.paragraphs:
+    if p.style.name.startswith("Heading") or p.style.name.startswith("Title"):
+        print(p.text)
