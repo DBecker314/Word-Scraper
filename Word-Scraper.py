2114d7738ff955519dd9d126e6a9074973733479
Dbecker314 <beckerdominicr@gmail.com>
2025-09-10 08:10:14 -0400

    Test of Single Docx

--- Word-Scraper.py
+++ Word-Scraper.py
@@ -1,4 +1,8 @@
 from docx import Document
+import pandas as pd
+Chunks = []
 document = Document('Proxy Review Sheet.docx')
 for paragraph in document.paragraphs:
-    print(paragraph.text)
+    Chunks.append(paragraph.text)
+Data = pd.DataFrame(Chunks)
+Data.to_csv('Lesson_Paragraphs')
