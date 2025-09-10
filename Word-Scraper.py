424d29116aedefe1940910ef00724c874c4da2df
Dbecker314 <beckerdominicr@gmail.com>
2025-09-10 08:50:32 -0400

    Added functionality for folders and files

--- Word-Scraper.py
+++ Word-Scraper.py
@@ -1,14 +1,19 @@
 from pathlib import Path
 from docx import Document
 import pandas as pd
-Folder = input("Input the Folder Path Here:")
-Chunks = []
-
-folder_path = Path(Folder)
-for item in folder_path.iterdir():
-    document = Document(item)
+input_path = input("Input Source File or Folder Here:")
+input_save = input("Output CSV File as:")
+chunks = []
+folder_path = Path(input_path)
+if folder_path.is_dir():
+    for item in folder_path.iterdir():
+        document = Document(item)
+        for paragraph in document.paragraphs:
+            chunks.append(paragraph.text)
+elif folder_path.is_file():
+    document = Document(folder_path)
     for paragraph in document.paragraphs:
-        Chunks.append(paragraph.text)
+        chunks.append(paragraph.text)
 
-Data = pd.DataFrame(Chunks)
-Data.to_csv('Lesson_Paragraphs')
+Data = pd.DataFrame(chunks)
+Data.to_csv(input_save)
