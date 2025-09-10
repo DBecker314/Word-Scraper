88c74bf6fbe1c69ae7482f7fb23133343a295489
Dbecker314 <beckerdominicr@gmail.com>
2025-09-10 09:48:35 -0400

    Added document column to output

--- Word-Scraper.py
+++ Word-Scraper.py
@@ -1,19 +1,32 @@
+# Imports
 from pathlib import Path
 from docx import Document
 import pandas as pd
-input_path = input("Input Source File or Folder Here:")
-input_save = input("Output CSV File as:")
-chunks = []
-folder_path = Path(input_path)
-if folder_path.is_dir():
-    for item in folder_path.iterdir():
-        document = Document(item)
-        for paragraph in document.paragraphs:
-            chunks.append(paragraph.text)
-elif folder_path.is_file():
-    document = Document(folder_path)
-    for paragraph in document.paragraphs:
-        chunks.append(paragraph.text)
 
-Data = pd.DataFrame(chunks)
-Data.to_csv(input_save)
+# Inputs
+input_path = input("Input DOCX File or Folder Here: ")
+input_save = input("Output CSV File as: ")
+
+# Create lists of all documents and paragraphs from input file or folder
+document_list = []
+paragraph_list = []
+file_path = Path(input_path)
+
+if file_path.is_dir():
+    for item in file_path.iterdir():
+        if item.suffix == '.docx':
+            document = Document(item)
+            for paragraph in document.paragraphs:
+                paragraph_list.append(paragraph.text)
+                document_list.append(item.stem)
+
+elif file_path.is_file():
+    document = Document(file_path)
+    for paragraph in document.paragraphs:
+        paragraph_list.append(paragraph.text)
+        document_list.append(file_path.stem)
+
+# Save to CSV
+data = {"Document":document_list,"Paragraph":paragraph_list}
+result = pd.DataFrame(data)
+result.to_csv(input_save)
