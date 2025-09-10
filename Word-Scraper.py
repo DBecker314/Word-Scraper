920e031350bbb6167728d00b63599525bca93d22
Dbecker314 <beckerdominicr@gmail.com>
2025-09-10 08:34:10 -0400

    Added folder accessibility and input function

--- Word-Scraper.py
+++ Word-Scraper.py
@@ -1,8 +1,14 @@
+from pathlib import Path
 from docx import Document
 import pandas as pd
+Folder = input("Input the Folder Path Here:")
 Chunks = []
-document = Document('Proxy Review Sheet.docx')
-for paragraph in document.paragraphs:
-    Chunks.append(paragraph.text)
+
+folder_path = Path(Folder)
+for item in folder_path.iterdir():
+    document = Document(item)
+    for paragraph in document.paragraphs:
+        Chunks.append(paragraph.text)
+
 Data = pd.DataFrame(Chunks)
 Data.to_csv('Lesson_Paragraphs')
